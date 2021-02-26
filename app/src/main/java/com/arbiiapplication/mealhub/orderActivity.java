0bed6cf8788fcf06c7dfd37dfc00c621c72b6b0f
Areeb Bin Azim <areebbinazim360@gmail.com>
2021-02-26 03:45:28 -0800

    Files uploaded of MealHub

---
+++ app/src/main/java/com/arbiiapplication/mealhub/orderActivity.java
@@ -0,0 +1,25 @@
+package com.arbiiapplication.mealhub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class orderActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.MSG);
+
+        TextView textView = findViewById(R.id.orderText);
+        textView.setText(message);
+
+    }
+}
